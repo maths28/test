ad21d8135579f568fe8fd42c0e2350affa74e951
mathieu <mathieu.boye28@gmail.com>
2017-06-01 13:40:09 +0200

    ok

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -1,31 +1,53 @@
 package com.example.demo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.concurrent.Future;
 
 @SpringBootApplication
 @RestController
+@EnableAsync
 public class TestApplication {
+
+	private Future<Double> response;
+
+	@Autowired
+	private UploadService uploadService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String test() throws IOException{
-		String text = "ceci est un test";
-		File f = new File("uploads");
-		f.mkdir();
-		f = new File(f.getPath() + "/e");
-		PrintWriter printWriter = new PrintWriter(f);
-		printWriter.println(text);
-		printWriter.close();
-		return f.getAbsolutePath();
+	@RequestMapping("/upload")
+	public String test(@RequestParam(name = "file", required = false)MultipartFile file, HttpServletResponse response) throws Exception{
+		if(this.response == null){
+			this.response = this.uploadService.upload(file);
+		}
+		DecimalFormat df = new DecimalFormat("0.00");
+
+		return df.format(this.getProgress())+"<a href='/upload'>Rafr</a>";
+//		response.sendRedirect("/progress");
 	}
+
+	private Double getProgress() throws Exception{
+		if(this.response.isDone()){
+			return this.response.get();
+		} else {
+			return this.uploadService.getProgress();
+		}
+	}
+
 }
---
+++ src/main/java/com/example/demo/UploadService.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.Future;
+
+/**
+ * Created by mathieu on 29/05/17.
+ */
+
+@Service("uploadService")
+public class UploadService {
+
+    private double progress;
+
+    @Async
+    public Future<Double> upload(MultipartFile file) throws IOException {
+        File f = new File("uploads/");
+        f.mkdirs();
+        f = new File("uploads/"+file.getOriginalFilename());
+        f.createNewFile();
+        FileOutputStream fos = new FileOutputStream(f);
+        byte[] read = new byte[1024];
+        double count = 0;
+        double total = file.getSize();
+        int readed = 0;
+        InputStream is = file.getInputStream();
+        while ((readed = is.read(read)) != -1){
+            fos.write(read);
+            count+=readed;
+            float countF = (float)count;
+            this.progress = countF / total * 100;
+        }
+        fos.close();
+        return new AsyncResult<Double>((double)100);
+    }
+
+    public double getProgress() {
+        return this.progress;
+    }
+
+    public void setProgress(double progress) {
+        this.progress = progress;
+    }
+}
